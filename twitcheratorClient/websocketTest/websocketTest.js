4240e156ee558e098102573f268c2f1ca573c591
Chris Dudziak <Chris.Dudziak@gmail.com>
2019-08-02 16:17:57 -0400

    WebSocketTest

--- twitcheratorClient/websocketTest/websocketTest.js
+++ twitcheratorClient/websocketTest/websocketTest.js
@@ -1,5 +1,42 @@
 var app = angular.module('myApp', []);
 
-app.controller('myCtrl',function($scope) {
+app.controller('myCtrl',function($scope, $interval) {
+   
+    $scope.message = "waiting...";
+    $scope.status = "Offline";
+    $scope.errorMessage = "";
+
+    $scope.socket;
+
+    $scope.retryIn = 5;
+
+    $scope.connect = function() {
+        $scope.socket = new WebSocket('ws://localhost:6030');
+
+        $scope.socket.addEventListener("message", function(event){
+            console.log("message from the server: " + event);       
+            $scope.message = event; 
+        }); 
     
+        $scope.socket.addEventListener("open", function(event){
+            console.log("message from the server: " + event);       
+            $scope.status = "Online"; 
+        }); 
+    
+        $scope.socket.addEventListener("error", function(event){
+            console.log("message from the server: " + event);       
+            $scope.errorMessage = "Error from : " + event.target.url;
+        });    
+                
+        $scope.retryIn = 5;    
+    }
+
+    $scope.increment = function() {
+        $scope.retryIn--;
+    }
+    
+    $interval($scope.connect, 5000);  
+    $interval($scope.increment, 1000);
+
 });
+    
