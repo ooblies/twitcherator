3efce73e7ae1faf528ade5343186ad17405013a1
austin <azmauldin@gmail.com>
2019-08-02 16:18:22 -0400

    here's johnny

---
+++ TwitcheratorServer/TwitcheratorServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TwitcheratorServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    IPHostEntry host = Dns.GetHostEntry("localhost");
+                    IPAddress iPAddress = host.AddressList.First();
+                    IPEndPoint ipe = new IPEndPoint(iPAddress, 6030);
+                    socket.Connect(ipe);
+                    socket.Send(Encoding.UTF8.GetBytes("hello"));
+                    Thread.Sleep(5000);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
